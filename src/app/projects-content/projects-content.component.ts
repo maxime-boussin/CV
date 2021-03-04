f5b35e61d3841d9a07ec44faed8aa8c49306de6b
maxime-boussin <maxime.boussin35@gmail.com>
2021-03-04 15:23:17 +0100

    Create projects tab and change background.

--- src/app/projects-content/projects-content.component.ts
+++ src/app/projects-content/projects-content.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {GithubService} from '../github.service';
 
+export interface Repository {
+  name: string;
+  last_modified: Date;
+  size: number;
+  url: string;
+}
+
 @Component({
   selector: 'app-projects-content',
   templateUrl: './projects-content.component.html',
   styleUrls: ['./projects-content.component.scss']
 })
 export class ProjectsContentComponent implements OnInit {
-  repos = [];
+  repos: Repository[] = [];
   constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
@@ -19,10 +26,24 @@ export class ProjectsContentComponent implements OnInit {
 
   initRepos(data: Array<any>): void {
     data.forEach(obj => {
-      if (obj.hasOwnProperty('full_name')){
-        this.repos.push(obj.full_name);
-      }
+      const repo: Repository = {
+        name: obj.full_name.substr(obj.full_name.indexOf('/') + 1),
+        last_modified: new Date(obj.updated_at),
+        size: obj.size,
+        url: obj.html_url
+      };
+      this.repos.push(repo);
     });
-    console.log(this.repos);
+    this.repos.sort((a: Repository, b: Repository) => {
+      return b.last_modified.getTime() - a.last_modified.getTime();
+    });
+  }
+
+  redirect(url: string): void {
+    window.open(url, '_blank');
+  }
+
+  convertSize(size: number): string {
+    return (size > 1000 ? (size > 100000 ? Math.round(size / 1000000) + 'Go' : Math.round(size / 1000) + 'Mo') : size + 'Ko');
   }
 }
